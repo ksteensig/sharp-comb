7187583cf18c084d3551cac3fb2de3851d6022ed
Kasper Steensig <kasper.steensig@gmail.com>
2018-01-14 20:02:06 +0100

    made samples directory

--- samples/Calculator/Calculator.cs
+++ samples/Calculator/Calculator.cs
@@ -1,8 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using MonadicParserCombinator;
 
-namespace MonadicParserCombinator
+namespace MonadicParserCombinator.Samples.Calculator
 {
     public class Addition
     {
