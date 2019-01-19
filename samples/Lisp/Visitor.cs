b5d62eab766a463e6f90f9ddf35f7ab9af2bd831
Kasper Steensig <kasper.steensig@gmail.com>
2019-01-19 17:28:44 +0100

    Add a name mangling visitor to allow for shadowing variables inside of scopes

--- samples/Lisp/Visitor.cs
+++ samples/Lisp/Visitor.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using MonadicParserCombinator;
 
 namespace MonadicParserCombinator.Samples.Lisp
@@ -52,9 +53,7 @@ namespace MonadicParserCombinator.Samples.Lisp
             Console.Write(" ");
             Console.Write(node.parameter.symbol);
             Console.Write(" ");
-            Console.Write("(");
             node.expression.Accept(this);
-            Console.Write(")");
         }
 
         public void Visit(LispLambda node)
@@ -63,10 +62,93 @@ namespace MonadicParserCombinator.Samples.Lisp
             Console.Write(" ");
             Console.Write(node.parameter.symbol);
             Console.Write(" ");
-            Console.Write("(");
             node.expression.Accept(this);
-            Console.Write(")");
         }
 
+    }
+
+    public class LispSymbolMangler : IVisitor
+    {
+        // (symbol, times spotted)
+        List<(string Symbol, int Count)> symbols;
+
+        public LispSymbolMangler()
+        {
+            symbols = new List<(string, int)>();
+        }
+
+        
+        public void Visit(LispList node)
+        {
+            foreach (var e in node.expressions)
+            {
+                e.Accept(this);
+            }
+        }
+
+        public void Visit(EmptyLispList node)
+        {
+        }
+
+        public void Visit(LispTerm node)
+        {
+        }
+
+        public void Visit(LispSymbol node)
+        {
+            var old_name = node.symbol;
+            var index = symbols.FindIndex(s => s.Symbol == old_name);
+
+            if (index != -1)
+            {
+                var postfix = symbols[index].Count;
+                node.symbol = old_name + postfix.ToString();
+            }
+        }
+
+        public void Visit(LispDefine node)
+        {
+            
+            var old_name = node.parameter.symbol;
+
+            var index = symbols.FindIndex(s => s.Symbol == old_name);
+
+            if (index != -1)
+            {
+                symbols[index] = (old_name, symbols[index].Count + 1);
+                var postfix = symbols[index].Count;
+                node.parameter = new LispSymbol(old_name + postfix.ToString());
+                node.expression.Accept(this);
+                symbols[index] = (old_name, symbols[index].Count - 1);
+            }
+            else
+            {
+                symbols.Add((old_name, 0));
+                node.expression.Accept(this);
+                symbols.Remove((old_name, 0));
+            }
+        }
+
+        public void Visit(LispLambda node)
+        {
+            var old_name = node.parameter.symbol;
+
+            var index = symbols.FindIndex(s => s.Symbol == old_name);
+
+            if (index != -1)
+            {
+                symbols[index] = (old_name, symbols[index].Count + 1);
+                var postfix = symbols[index].Count;
+                node.parameter = new LispSymbol(old_name + postfix.ToString());
+                node.expression.Accept(this);
+                symbols[index] = (old_name, symbols[index].Count - 1);
+            }
+            else
+            {
+                symbols.Add((old_name, 0));
+                node.expression.Accept(this);
+                symbols.Remove((old_name, 0));
+            }
+        }
     }
 }
