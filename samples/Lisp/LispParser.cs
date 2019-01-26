90153e32c6dd1b5eacbc2408fca62d853ea5f8d0
Kasper Steensig <kasper.steensig@gmail.com>
2019-01-26 23:45:24 +0100

    Finish identifier parser and begin working on datum parsers

--- samples/Lisp/LispParser.cs
+++ samples/Lisp/LispParser.cs
@@ -28,9 +28,10 @@ namespace MonadicParserCombinator.Samples.Lisp
     public class LispVariableDefinition2 : LispVariableDefinition { }
     public class LispVariableDefinition3 : LispVariableDefinition { }
 
+    public class LispExpression : LispForm { }
     public class LispVariable
     {
-        string identifier;
+        public string identifier;
 
         public LispVariable(string id)
         {
@@ -38,6 +39,93 @@ namespace MonadicParserCombinator.Samples.Lisp
         }
     }
 
+    public class LispDatum { }
+
+    public class LispBoolean : LispDatum
+    {
+        public bool boolean;
+
+        public LispBoolean(bool boolean)
+        {
+            this.boolean = boolean;
+        }
+
+        public LispBoolean(string boolean)
+        {
+            if (boolean == "#f")
+            {
+                this.boolean = false;
+            }
+            else
+            {
+                this.boolean = true;
+            }
+        }
+    }
+
+    public class LispNumber : LispDatum { }
+
+    public class LispInt : LispNumber
+    {
+        public int value;
+
+        public LispInt(int value)
+        {
+            this.value = value;
+        }
+    }
+
+    public class LispDecimal : LispNumber
+    {
+        public double value;
+
+        public LispDecimal(double value)
+        {
+            this.value = value;
+        }
+    }
+
+    public class LispChar : LispDatum
+    {
+        public string character;
+
+        public LispChar(string character)
+        {
+            this.character = character;
+        }
+    }
+
+    public class LispString : LispDatum
+    {
+        public string str;
+
+        public LispString(string str)
+        {
+            this.str = str;
+        }
+    }
+
+    public class LispList : LispDatum
+    {
+        public LispDatum head;
+        public LispDatum tail;
+
+        public LispList(LispDatum head, LispDatum tail)
+        {
+            this.head = head;
+            this.tail = tail;
+        }
+    }
+
+    public class LispVector : LispDatum
+    {
+        public List<LispDatum> vector;
+
+        public LispVector(List<LispDatum> vector)
+        {
+            this.vector = vector;
+        }
+    }
 
 
     public static class LispParser
@@ -65,26 +153,27 @@ namespace MonadicParserCombinator.Samples.Lisp
         static Parser<char> Newline =
             from nl in Parser.Char('\n')
             select nl;
-        /*
-                static Parser<string> IdentifierParser = Parser.EitherOf(new List<string>
-                {
-                    from i in InitialParser
-                    from s in SubsequentParser.Many()
-                    select i,
 
-                    Parser.Char('+').Text(),
-                    Parser.Char('-').Text(),
-                    Parser.MatchString("...")
-                });
-        */
-        static Parser<string> InitialParser = Parser.EitherOf(new List<Parser<string>>
-        {
-            Parser.MatchRegex(new Regex("a-z")),
-            InitialCharParser.Text()
-        });
+        static Parser<LispVariable> VariableParser =
+            from i in IdentifierParser
+            select new LispVariable(i);
 
-        static Parser<IEnumerable<char>> InitialCharParser =
-            from c in Parser.EitherOf(new List<Parser<char>>{
+        static Parser<string> IdentifierParser =
+            from i in Parser.EitherOf(new List<Parser<string>>
+            {
+                from i in InitialParser
+                from s in SubsequentParser.Many()
+                select string.Concat(i.ToString(), s),
+                Parser.MatchRegex(new Regex("+")),
+                Parser.MatchRegex(new Regex("-")),
+                Parser.MatchRegex(new Regex("..."))
+            })
+            select i;
+
+        static Parser<char> InitialParser =
+            from c in Parser.EitherOf(new List<Parser<char>>
+            {
+                LetterParser,
                 Parser.Char('!'),
                 Parser.Char('$'),
                 Parser.Char('%'),
@@ -100,22 +189,47 @@ namespace MonadicParserCombinator.Samples.Lisp
                 Parser.Char('_'),
                 Parser.Char('^')
             })
-            select Parser.ReturnIEnumerable(c);
+            select c;
 
-
-        static Parser<string> SubsequentParser =
-            from s in Parser.EitherOf(new List<Parser<string>> {
-                InitialParser, SubsequentCharParser.Text()})
-            select s;
-
-        static Parser<IEnumerable<char>> SubsequentCharParser =
+        static Parser<char> SubsequentParser =
             from c in Parser.EitherOf(new List<Parser<char>>
             {
+                InitialParser,
                 Parser.Digit,
                 Parser.Char('.'),
                 Parser.Char('+'),
                 Parser.Char('-')
             })
-            select Parser.ReturnIEnumerable(c);
+            select c;
+
+        static Parser<char> LetterParser =
+            from l in Parser.MatchRegex(new Regex("a-z"))
+            select l.First();
+
+        static Parser<LispInt> IntParser =
+            from i in Parser.MatchRegex(new Regex("0-90-9*"))
+            select new LispInt(int.Parse(i));
+
+        static Parser<LispDecimal> DecimalParser =
+            from d in Parser.MatchRegex(new Regex("0-9*.0-90-9*"))
+            select new LispDecimal(Double.Parse(d));
+
+        static Parser<LispBoolean> BooleanParser =
+            from b in Parser.EitherOf(new List<Parser<string>>
+            {
+                Parser.MatchString("#f"),
+                Parser.MatchString("#t")
+            })
+            select new LispBoolean(b);
+        /*
+                static Parser<LispChar> CharParser =
+                    from s in Parser.EitherOf(new List<Parser>
+                    {
+                        Parser.MatchString(@"#\newline"),
+                        Parser.MatchString(@"#\space"),
+                        Parser.MatchRegex(new Regex(@"#\a-z")),
+                        Parser.MatchRegex(new Regex(@"#\A-Z"))
+                    })
+                    */
     }
 }
